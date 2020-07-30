8f37607256265b47da9f7dbf53527a891bcf566b
= <=>
2020-07-30 12:41:53 +0700

    assignment 3

---
+++ Java/TestingSystem_Assignment_3/src/Account.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class Account {
+
+	    int id;
+	    String email;
+	    String username;
+	    String fullname;
+	    Department department;
+	    Position position;
+	    LocalDate createDate;
+	    Group[] groups;
+
+}
---
+++ Java/TestingSystem_Assignment_3/src/Exercise3.java
@@ -0,0 +1,29 @@
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		question1();
+		question2();
+		question3();
+	}
+	//Q1//
+		public static void question1() {
+		Integer salary = 5000;
+		System.out.printf("%2.2f", (float) salary);
+		}
+	//Q2//
+		public static void question2() {
+		String s1 = "1234567";
+		Integer s = Integer.parseInt(s1);
+		System.out.println(s);
+		}
+	//Q3//
+		public static void question3() {
+			Integer a = 1234567;
+			int b = a.intValue();
+			System.out.println(b);
+		}
+
+	}
+
+
